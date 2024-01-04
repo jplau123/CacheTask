dfc8e388e752e94fe763a52ba1d22b58eb62ffd0
ViliusSe <v.senkus@adform.com>
2024-01-04 14:03:32 +0200

    Entities and DTO's created

---
+++ src/Application/DTOs/Requests/AppendPairRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.DTOs.Requests;
+
+public record AppendPairRequest
+{
+    public string Key { get; set; } = "";
+    public List<object> Value { get; set; } = [];
+    public TimeSpan ExpirationPeriod { get; set; }
+}
---
+++ src/Application/DTOs/Requests/CreatePairRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.DTOs.Requests;
+
+public record CreatePairRequest
+{
+    public string Key { get; set; } = "";
+    public List<object> Value { get; set; } = [];
+    public TimeSpan ExpirationPeriod { get; set; }
+}
