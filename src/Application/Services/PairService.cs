e84623768b1336ee55f9b4dadaad73b50d1f765e
ViliusSe <v.senkus@adform.com>
2024-01-04 16:26:24 +0200

    Updates

--- src/Application/Services/PairService.cs
+++ src/Application/Services/PairService.cs
@@ -1,29 +1,39 @@
 ﻿using Application.DTOs.Requests;
 using Application.DTOs.Responses;
 using Application.Interfaces;
+using Domain.Interfaces;
+using Microsoft.Extensions.Configuration;
 
-namespace Application.Services
+namespace Application.Services;
+
+public class PairService : IPairService
 {
-    public class PairService : IPairService
+    private readonly IPairRepository _pairRepository;
+    private readonly IConfiguration _configuration;
+
+    public PairService(IPairRepository pairRepository, IConfiguration configuration)
     {
-        public Task<AppendPairResponse> Append(AppendPairRequest request)
-        {
-            throw new NotImplementedException();
-        }
+        _pairRepository = pairRepository;
+        _configuration = configuration;
+    }
 
-        public Task<CreatePairResponse> Create(CreatePairRequest request)
-        {
-            throw new NotImplementedException();
-        }
+    public Task<AppendPairResponse> Append(AppendPairRequest request)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task Delete(string key)
-        {
-            throw new NotImplementedException();
-        }
+    public Task<CreatePairResponse> Create(CreatePairRequest request)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<GetPairResponse> Get(string key)
-        {
-            throw new NotImplementedException();
-        }
+    public Task Delete(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<GetPairResponse> Get(string key)
+    {
+        throw new NotImplementedException();
     }
 }
