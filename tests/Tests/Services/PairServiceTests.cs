3ca1c9fb4f79e6597cf934776fd73a6ae4617fe3
a.ptakauskas <a.ptakauskas@adform.com>
2024-01-05 17:47:24 +0200

    service delete method tests edit

--- tests/Tests/Services/PairServiceTests.cs
+++ tests/Tests/Services/PairServiceTests.cs
@@ -2,9 +2,9 @@
 using Domain.Interfaces;
 using Application.Services;
 using AutoFixture;
-using Application.Interfaces;
 using Domain.Entities;
 using Microsoft.Extensions.Configuration;
+using Domain.Exceptions;
 
 
 public class PairServiceTests
@@ -13,13 +13,13 @@ public class PairServiceTests
     private readonly PairService _pairService;
     private readonly IFixture _fixture;
     private readonly IConfiguration _configuration;
-    public PairServiceTests(IConfiguration configuration)
+   
+    public PairServiceTests()
     {
-
         _testRepository = new Mock<IPairRepository>();
-        _configuration = configuration;
-        _pairService = new PairService(_testRepository.Object, _configuration);
         _fixture = new Fixture();
+        _configuration = new ConfigurationBuilder().Build();
+        _pairService = new PairService(_testRepository.Object, _configuration);
     }
 
     [Fact]
@@ -27,29 +27,16 @@ public class PairServiceTests
     {
         var key = _fixture.Create<string>();
         var keyValue = new PairEntity { Key = key };
-
-        var configuration = new Mock<IConfiguration>();
-        
-
         _testRepository.Setup(repo => repo.GetByKey(key)).ReturnsAsync(keyValue);
-
-        var pairService = new PairService(_testRepository.Object, configuration.Object);
-
-        await pairService.Delete(key);
-
+        await _pairService.Delete(key);
         _testRepository.Verify(repo => repo.Delete(key), Times.Once);
     }
 
-
-    //[Fact]
-    //public async Task Delete_NonExistingKey_ThrowsNotFoundException()
-    //{
-    //    var key = "nonExistingKey";
-    //    var pairRepositoryMock = new Mock<IPairRepository>();
-    //    pairRepositoryMock.Setup(repo => repo.GetByKey(key)).ReturnsAsync((YourValueType)null); 
-    //    var yourClass = new PairService(pairRepositoryMock.Object);
-
-
-    //    await Assert.ThrowsAsync<NotFoundException>(async () => await yourClass.Delete(key));
-    //}
+    [Fact]
+    public async Task Delete_NotExistingKeyGiven_ThrowsNotFoundException()
+    {
+        var key = _fixture.Create<string>();
+        _testRepository.Setup(repo => repo.GetByKey(key)).Returns(Task.FromResult<PairEntity?>(null));
+        await Assert.ThrowsAsync<NotFoundException>(() => _pairService.Delete(key));
+    }
 }
